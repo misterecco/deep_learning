bd18f6bb955917930cce0d0b6dd432ea25bccd02
Tomasz Kępa <tomek@qed.ai>
2017-06-02 00:12:09 +0200

    Task #2: refactor file queue

--- task_02/task_02.py
+++ task_02/task_02.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 
 
 DATASET_PATH = './spacenet2'
-TRAINING_SET_FILENAMES = 'training_set.txt'
-VALIDATION_SET_FILENAMES = 'validation_set.txt'
+TRAINING_SET = 'training_set.txt'
+VALIDATION_SET = 'validation_set.txt'
+
 BATCH_SIZE = 8
 IMAGE_SIZE = 650
 NUM_CHANELS = 3
@@ -14,94 +15,81 @@ def prepare_file_list(file):
     f = open(file, 'r')
     return [l.strip() for l in f]
 
-def prepare_img_list(filelist):
+def prepare_img_list(file_list):
     img_dir = DATASET_PATH + "/images/"
-    return [img_dir + f for f in filelist]
+    return [img_dir + f for f in file_list]
 
-def prepare_ht_list(filelist):
+def prepare_ht_list(file_list):
     ht_dir = DATASET_PATH + "/images/"
-    return [ht_dir + f for f in filelist]
+    return [ht_dir + f for f in file_list]
 
-train_paths = prepare_file_list(TRAINING_SET_FILENAMES)
-val_paths = prepare_file_list(VALIDATION_SET_FILENAMES)
-
-train_paths_img = prepare_img_list(train_paths)
-train_paths_ht = prepare_ht_list(train_paths)
-
-val_paths_img = prepare_img_list(val_paths)
-val_paths_ht = prepare_ht_list(val_paths)
-
-
-train_paths_img_tensor = tf.convert_to_tensor(train_paths_img, dtype=tf.string)
-train_paths_ht_tensor = tf.convert_to_tensor(train_paths_ht, dtype=tf.string)
-
-val_paths_img_tensor = tf.convert_to_tensor(val_paths_img, dtype=tf.string)
-val_paths_ht_tensor = tf.convert_to_tensor(val_paths_ht, dtype=tf.string)
-
-
-train_input_queue = tf.train.slice_input_producer(
-    [train_paths_img_tensor, train_paths_ht_tensor],
-    shuffle=True
-)
-
-val_input_queue = tf.train.slice_input_producer(
-    [val_paths_img_tensor, val_paths_ht_tensor],
-    shuffle=True
-)
-
-# TODO: finish pipeline also for validation set
-
-train_image_content = tf.read_file(train_input_queue[0])
-train_heatmap_content = tf.read_file(train_input_queue[1])
-
-train_image = tf.image.decode_jpeg(train_image_content, channels=NUM_CHANELS)
-train_heatmap = tf.image.decode_jpeg(train_heatmap_content, channels=NUM_CHANELS)
-
-
-train_image.set_shape([IMAGE_SIZE, IMAGE_SIZE, NUM_CHANELS])
-train_heatmap.set_shape([IMAGE_SIZE, IMAGE_SIZE, NUM_CHANELS])
-
-
-train_image_batch, train_heatmap_batch = tf.train.batch(
-    [train_image, train_heatmap],
-    batch_size = BATCH_SIZE
-)
-
-
-val_image_content = tf.read_file(val_input_queue[0])
-val_heatmap_content = tf.read_file(val_input_queue[1])
-
-val_image = tf.image.decode_jpeg(val_image_content, channels=NUM_CHANELS)
-val_heatmap = tf.image.decode_jpeg(val_heatmap_content, channels=NUM_CHANELS)
-
-
-val_image.set_shape([IMAGE_SIZE, IMAGE_SIZE, NUM_CHANELS])
-val_heatmap.set_shape([IMAGE_SIZE, IMAGE_SIZE, NUM_CHANELS])
-
-
-val_image_batch, val_heatmap_batch = tf.train.batch(
-    [val_image, val_heatmap],
-    batch_size = BATCH_SIZE
-)
+def prepare_file_paths(file):
+    file_list = prepare_file_list(file)
+    return (prepare_img_list(file_list), prepare_ht_list(file_list))
 
 
 
-with tf.Session() as sess:
+class Trainer():
+    def prepare_queues(self):
+        train_paths = prepare_file_paths(TRAINING_SET)
+        val_paths = prepare_file_paths(VALIDATION_SET)
 
-    tf.global_variables_initializer().run()
+        train_paths_tensors = [tf.convert_to_tensor(path, dtype=tf.string) for path in train_paths]
+        val_paths_tensors = [tf.convert_to_tensor(path, dtype=tf.string) for path in val_paths]
 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-    
-    print('From the train set:')
-    for _ in range(1):
-        print(sess.run(train_image_batch))
+        train_input_queue = tf.train.slice_input_producer(
+            train_paths_tensors,
+            shuffle=True
+        )
+        val_input_queue = tf.train.slice_input_producer(
+            val_paths_tensors,
+            shuffle=True
+        )
 
-    print('From the val set:')
-    for _ in range(1):
-        print(sess.run(val_image_batch))
+        train_file_contents = [tf.read_file(file) for file in train_input_queue]
+        val_file_contents = [tf.read_file(file) for file in val_input_queue]
 
-    coord.request_stop()
-    coord.join(threads)
+        train_images = [tf.image.decode_jpeg(img, channels=NUM_CHANELS) for img in train_file_contents]
+        val_images = [tf.image.decode_jpeg(img, channels=NUM_CHANELS) for img in val_file_contents]
 
-    sess.close()
+        for image in train_images:
+            image.set_shape([IMAGE_SIZE, IMAGE_SIZE, NUM_CHANELS])
+        for image in val_images:
+            image.set_shape([IMAGE_SIZE, IMAGE_SIZE, NUM_CHANELS])
+
+        self.train_image_batches = tf.train.batch(
+            train_images,
+            batch_size = BATCH_SIZE
+        )
+        self.val_image_batches = tf.train.batch(
+            val_images,
+            batch_size = BATCH_SIZE
+        )
+
+    def train(self):
+        self.prepare_queues()
+
+        with tf.Session() as self.sess:
+            tf.global_variables_initializer().run()
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
+            
+            print('From the train set:')
+            for _ in range(1):
+                print(self.sess.run(self.train_image_batches[0]))
+
+            print('From the val set:')
+            for _ in range(1):
+                print(self.sess.run(self.val_image_batches[0]))
+
+            coord.request_stop()
+            coord.join(threads)
+
+            self.sess.close()
+
+
+if __name__ == '__main__':
+    trainer = Trainer()
+
+    trainer.train()
